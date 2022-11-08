b3b9f71cdd56a95032eb7b55e846035c7058e5b2
hrr2000 <thedragonea@gmail.com>
2022-11-08 20:41:37 +0200

    redeploy

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -74,9 +74,8 @@ export default async function handler(
       subject: "Your Application at NATI Institute", // Subject line
       html: 'We Have Received Your Message and Will Get Back to You Shortly.'
     });
-  } catch (error: any) {
-    console.log(error);
-    return res.status(500).json({ error: error.message });
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to Submit the Application' });
   }
 
 
