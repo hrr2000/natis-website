c4cf5a06a3cb0802115425b805f17ed693d7f3aa
hrr2000 <thedragonea@gmail.com>
2023-01-11 19:10:33 +0200

    fix: fix the deployment

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     domains: ["localhost", "images.unsplash.com", "23.23.48.83", "nati-esl-usa.org"],
   },
   i18n: {
-    // locales: ['en-US'],
-    locales: ["en-US", "ar-SA", "fr-FR", "es-ES"],
+    locales: ['en-US'],
+    // locales: ["en-US", "ar-SA", "fr-FR", "es-ES"],
     defaultLocale: "en-US",
   },
   async headers() {
